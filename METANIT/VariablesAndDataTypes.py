01a0f8e50a97c28834a2f97be9ecfbe32bea901a
Victoria <victoria1orlova@gmail.com>
2024-07-12 17:35:45 -0600

    Added few chapters

---
+++ METANIT/VariablesAndDataTypes.py
@@ -0,0 +1,35 @@
+#bool, int, float, complex и str.
+
+'''
+bool    - True/False
+int     - 1, 4, 8, 50.
+float   - 1.2 or 34.76
+complex - 1+2j
+str     - "Hello World!"
+'''
+
+#Консольный ввод и вывод
+
+print("Hello Mila", end=" ")
+print("Hello Father", end=" ")      #параметр end
+print("Hello Viktoria")
+
+print("Hello Mila", end=", ")
+print("hello Father", end=" and ")      #параметр end
+print("hello Viktoria")
+
+name = input("Enter your name: ")
+age = input("Your age: ")
+print(f"Your name: {name}")
+print(f"Your age: {age}")
+
+
+'''
+** степень
+
+'''
+
+print(7 / 2)  # 3.5
+print(7 // 2)  # 3
+
+print(round(2.51))  # 3
