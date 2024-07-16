13e4dcc1fcd237d0c1776296192b5d3d3a6aa7e9
Victoria <victoria1orlova@gmail.com>
2024-07-16 14:26:43 -0600

    Workshop on the topic 'Cycles while, for'

---
+++ METANIT/2_7.py
@@ -0,0 +1,26 @@
+
+count = int(input("Enter any number I should count to: "))
+for n in range(count):
+    print(n, end=" ")
+
+'''
+number = 1
+ 
+while number < 5:
+    print(f"number = {number}")
+    number += 1
+else:
+    print(f"number = {number}. Работа цикла завершена")
+print("Работа программы завершена")
+
+message = str(input("Enter any word: "))
+
+for c in message:
+    print(c)
+
+number = 1
+while number < 5:
+    print(f"number = {number}")
+    number += 1
+print("Работа программы завершена")
+'''
