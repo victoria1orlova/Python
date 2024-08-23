09c14a0b7569b288097576dc8d7d59df1817c88e
Victoria <victoria1orlova@gmail.com>
2024-08-23 14:02:56 -0600

    Developed lineInFrame

--- proj/lineInFrame.py
+++ proj/lineInFrame.py
@@ -1,5 +1,27 @@
 #функция которая принимает строку и на выходе строка в рамке
 line = str(input("Enter something: "))
+
+def drawUnicodeBox(line):
+    length = len(line)
+
+    # верхняя граница
+    topBorder = "\u2554" + "\u2550" * (length) + "\u2557"
+    print(topBorder)
+
+    # боковые границы 
+    
+    middle = "\u2551" + line + "\u2551"
+    print(middle)
+
+    # нижняя граница
+    bottomBorder = "\u255a" + "\u2550" * (length) + "\u255d"
+    print(bottomBorder)
+
+drawUnicodeBox(line)
+
+
+'''
+line = str(input("Enter something: "))
 length = len(line)
 
 def lineInFrame(line):
@@ -18,3 +40,5 @@ def lineInFrame(line):
     print(framedStr)
 
 lineInFrame(line)
+
+'''
